1df8eb0df002a1c8109a18ee42c89717267cb3f8
andrelesch <andre.lesch@aol.com>
2021-10-07 23:53:32 +0200

    Chantelle se uitgelosde goed is nou werkend

---
+++ AccountSystemWebSpringBoot/src/main/java/za/ac/nwu/web/sb/RestServiceApplication.java
@@ -0,0 +1,16 @@
+package za.ac.nwu.web.sb;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class RestServiceApplication extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
