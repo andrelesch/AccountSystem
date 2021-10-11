9722d2affa501f332b5ff4a546a3a4018acd288b
andrelesch <andre.lesch@aol.com>
2021-10-12 00:29:20 +0200

    Update 17.0

--- AccountSystemLogic/src/main/java/za/ac/nwu/logic/flow/impl/FetchDiscovery_AccountFlowImpl.java
+++ AccountSystemLogic/src/main/java/za/ac/nwu/logic/flow/impl/FetchDiscovery_AccountFlowImpl.java
@@ -3,13 +3,9 @@ package za.ac.nwu.logic.flow.impl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import za.ac.nwu.domain.dto.Discovery_Account_Dto;
-import za.ac.nwu.domain.persistence.Discovery_Account;
 import za.ac.nwu.logic.flow.FetchDiscovery_AccountFlow;
 import za.ac.nwu.translator.Discovery_AccountTranslator;
-
 import javax.transaction.Transactional;
-import java.time.LocalDate;
-import java.util.ArrayList;
 import java.util.List;
 
 @Transactional
@@ -24,9 +20,7 @@ public class FetchDiscovery_AccountFlowImpl implements FetchDiscovery_AccountFlo
     }
     @Override
     public List<Discovery_Account_Dto> getAllDiscovery_Account() {
-        List<Discovery_Account_Dto>Discovery_AccountDtos = new ArrayList<>();
-        Discovery_Account.add(new Discovery_Account_Dto("Miles", "Miles", LocalDate.now()));
-        return Discovery_AccountDtos;
+        return discovery_accountTranslator.getAllDiscovery_Accounts();
     }
 
 
